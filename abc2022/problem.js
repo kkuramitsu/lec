2777d5bad98c36472e3ce8358be9f222ec64dac6
kkuramitsu <kkuramitsu@gmail.com>
2022-09-21 23:22:02 +0900

    abc2022

---
+++ abc2022/problem.js
@@ -0,0 +1,104 @@
+problems = [
+    //腕試し
+    ["ABC269 A問題", "https://atcoder.jp/contests/abc269/tasks/abc269_a", ""],
+    ["ABC269 B問題", "https://atcoder.jp/contests/abc269/tasks/abc269_b", ""],
+    ["ABC269 C問題", "https://atcoder.jp/contests/abc269/tasks/abc269_c", ""],
+    ["ABC269 D問題", "https://atcoder.jp/contests/abc269/tasks/abc269_d", ""],
+
+    ["ABC270 A問題", "https://atcoder.jp/contests/abc270/tasks/abc270_a", ""],
+    ["ABC270 B問題", "https://atcoder.jp/contests/abc270/tasks/abc270_b", ""],
+    ["ABC270 C問題", "https://atcoder.jp/contests/abc270/tasks/abc270_c", ""],
+    ["ABC270 D問題", "https://atcoder.jp/contests/abc270/tasks/abc270_d", ""],
+
+    ["ABC271 A問題", "https://atcoder.jp/contests/abc271/tasks/abc271_a", ""],
+    ["ABC271 B問題", "https://atcoder.jp/contests/abc271/tasks/abc271_b", ""],
+    ["ABC271 C問題", "https://atcoder.jp/contests/abc271/tasks/abc271_c", ""],
+    ["ABC271 D問題", "https://atcoder.jp/contests/abc271/tasks/abc271_d", ""],
+
+    ["ABC272 A問題", "https://atcoder.jp/contests/abc272/tasks/abc272_a", ""],
+    ["ABC272 B問題", "https://atcoder.jp/contests/abc272/tasks/abc272_b", ""],
+    ["ABC272 C問題", "https://atcoder.jp/contests/abc272/tasks/abc272_c", ""],
+    ["ABC272 D問題", "https://atcoder.jp/contests/abc272/tasks/abc272_d", ""],
+
+    ["ABC273 A問題", "https://atcoder.jp/contests/abc273/tasks/abc273_a", ""],
+    ["ABC273 B問題", "https://atcoder.jp/contests/abc273/tasks/abc273_b", ""],
+    ["ABC273 C問題", "https://atcoder.jp/contests/abc273/tasks/abc273_c", ""],
+    ["ABC273 D問題", "https://atcoder.jp/contests/abc273/tasks/abc273_d", ""],
+
+    ["ABC274 A問題", "https://atcoder.jp/contests/abc274/tasks/abc274_a", ""],
+    ["ABC274 B問題", "https://atcoder.jp/contests/abc274/tasks/abc274_b", ""],
+    ["ABC274 C問題", "https://atcoder.jp/contests/abc274/tasks/abc274_c", ""],
+    ["ABC274 D問題", "https://atcoder.jp/contests/abc274/tasks/abc274_d", ""],
+
+    ["ABC275 A問題", "https://atcoder.jp/contests/abc275/tasks/abc275_a", ""],
+    ["ABC275 B問題", "https://atcoder.jp/contests/abc275/tasks/abc275_b", ""],
+    ["ABC275 C問題", "https://atcoder.jp/contests/abc275/tasks/abc275_c", ""],
+    ["ABC275 D問題", "https://atcoder.jp/contests/abc275/tasks/abc275_d", ""],
+
+    ["ABC276 A問題", "https://atcoder.jp/contests/abc276/tasks/abc276_a", ""],
+    ["ABC276 B問題", "https://atcoder.jp/contests/abc276/tasks/abc276_b", ""],
+    ["ABC276 C問題", "https://atcoder.jp/contests/abc276/tasks/abc276_c", ""],
+    ["ABC276 D問題", "https://atcoder.jp/contests/abc276/tasks/abc276_d", ""],
+
+    ["ABC277 A問題", "https://atcoder.jp/contests/abc277/tasks/abc277_a", ""],
+    ["ABC277 B問題", "https://atcoder.jp/contests/abc277/tasks/abc277_b", ""],
+    ["ABC277 C問題", "https://atcoder.jp/contests/abc277/tasks/abc277_c", ""],
+    ["ABC277 D問題", "https://atcoder.jp/contests/abc277/tasks/abc277_d", ""],
+
+    ["ABC278 A問題", "https://atcoder.jp/contests/abc278/tasks/abc278_a", ""],
+    ["ABC278 B問題", "https://atcoder.jp/contests/abc278/tasks/abc278_b", ""],
+    ["ABC278 C問題", "https://atcoder.jp/contests/abc278/tasks/abc278_c", ""],
+    ["ABC278 D問題", "https://atcoder.jp/contests/abc278/tasks/abc278_d", ""],
+
+    ["ABC279 A問題", "https://atcoder.jp/contests/abc279/tasks/abc279_a", ""],
+    ["ABC279 B問題", "https://atcoder.jp/contests/abc279/tasks/abc279_b", ""],
+    ["ABC279 C問題", "https://atcoder.jp/contests/abc279/tasks/abc279_c", ""],
+    ["ABC279 D問題", "https://atcoder.jp/contests/abc279/tasks/abc279_d", ""],
+
+    ["ABC280 A問題", "https://atcoder.jp/contests/abc280/tasks/abc280_a", ""],
+    ["ABC280 B問題", "https://atcoder.jp/contests/abc280/tasks/abc280_b", ""],
+    ["ABC280 C問題", "https://atcoder.jp/contests/abc280/tasks/abc280_c", ""],
+    ["ABC280 D問題", "https://atcoder.jp/contests/abc280/tasks/abc280_d", ""],
+
+    // ["ABC281 A問題", "https://atcoder.jp/contests/abc281/tasks/abc281_a", ""],
+    // ["ABC281 B問題", "https://atcoder.jp/contests/abc281/tasks/abc281_b", ""],
+    // ["ABC281 C問題", "https://atcoder.jp/contests/abc281/tasks/abc281_c", ""],
+    // ["ABC281 D問題", "https://atcoder.jp/contests/abc281/tasks/abc281_d", ""],
+
+    // ["ABC282 A問題", "https://atcoder.jp/contests/abc282/tasks/abc282_a", ""],
+    // ["ABC282 B問題", "https://atcoder.jp/contests/abc282/tasks/abc282_b", ""],
+    // ["ABC282 C問題", "https://atcoder.jp/contests/abc282/tasks/abc282_c", ""],
+    // ["ABC282 D問題", "https://atcoder.jp/contests/abc282/tasks/abc282_d", ""],
+
+    // ["ABC283 A問題", "https://atcoder.jp/contests/abc283/tasks/abc283_a", ""],
+    // ["ABC283 B問題", "https://atcoder.jp/contests/abc283/tasks/abc283_b", ""],
+    // ["ABC283 C問題", "https://atcoder.jp/contests/abc283/tasks/abc283_c", ""],
+    // ["ABC283 D問題", "https://atcoder.jp/contests/abc283/tasks/abc283_d", ""],
+
+    // ["ABC284 A問題", "https://atcoder.jp/contests/abc284/tasks/abc284_a", ""],
+    // ["ABC284 B問題", "https://atcoder.jp/contests/abc284/tasks/abc284_b", ""],
+    // ["ABC284 C問題", "https://atcoder.jp/contests/abc284/tasks/abc284_c", ""],
+    // ["ABC284 D問題", "https://atcoder.jp/contests/abc284/tasks/abc284_d", ""],
+
+    // ["ABC285 A問題", "https://atcoder.jp/contests/abc285/tasks/abc285_a", ""],
+    // ["ABC285 B問題", "https://atcoder.jp/contests/abc285/tasks/abc285_b", ""],
+    // ["ABC285 C問題", "https://atcoder.jp/contests/abc285/tasks/abc285_c", ""],
+    // ["ABC285 D問題", "https://atcoder.jp/contests/abc285/tasks/abc285_d", ""],
+
+    // ["ABC286 A問題", "https://atcoder.jp/contests/abc286/tasks/abc286_a", ""],
+    // ["ABC286 B問題", "https://atcoder.jp/contests/abc286/tasks/abc286_b", ""],
+    // ["ABC286 C問題", "https://atcoder.jp/contests/abc286/tasks/abc286_c", ""],
+    // ["ABC286 D問題", "https://atcoder.jp/contests/abc286/tasks/abc286_d", ""],
+
+    // ["ABC287 A問題", "https://atcoder.jp/contests/abc287/tasks/abc287_a", ""],
+    // ["ABC287 B問題", "https://atcoder.jp/contests/abc287/tasks/abc287_b", ""],
+    // ["ABC287 C問題", "https://atcoder.jp/contests/abc287/tasks/abc287_c", ""],
+    // ["ABC287 D問題", "https://atcoder.jp/contests/abc287/tasks/abc287_d", ""],
+
+    // ["ABC288 A問題", "https://atcoder.jp/contests/abc288/tasks/abc288_a", ""],
+    // ["ABC288 B問題", "https://atcoder.jp/contests/abc288/tasks/abc288_b", ""],
+    // ["ABC288 C問題", "https://atcoder.jp/contests/abc288/tasks/abc288_c", ""],
+    // ["ABC288 D問題", "https://atcoder.jp/contests/abc288/tasks/abc288_d", ""],
+
+]
+
---
+++ abc2022/script.js
@@ -0,0 +1,126 @@
+// GET のデータを配列にして返す
+function GetQueryString() {
+    var result = {};
+    if (1 < window.location.search.length) {
+        var query = window.location.search.substring(1);
+        var parameters = query.split('&');
+        for (var i = 0; i < parameters.length; i++) {
+            var element = parameters[i].split('=');
+            var paramName = decodeURIComponent(element[0]);
+            var paramValue = decodeURIComponent(element[1]);
+            result[paramName] = paramValue;
+        }
+    }
+    return result;
+}
+
+window.onload = function () {
+    const query = GetQueryString();
+    if (query["uid"]) {
+        atcoder_id.value = query["uid"];
+    }
+
+    // 問題一覧を表示
+    var cells = {
+    }
+
+    function tail(ss) {
+        return ss[ss.length - 1];
+    }
+
+    function count(d, key, n) {
+        if (key in d) {
+            d[key] += n;
+        }
+        else {
+            d[key] = n;
+        }
+    }
+
+    var unit_count = -1;
+    var problem_count = 0;
+    for (var i = 0; i < problems.length; i++) {
+        if (problems[i].length == 1) continue;
+        var title = problems[i][0];
+        var url = problems[i][1]; // url
+        problem_count += 1;
+        var problem_id = tail(url.split('/'));
+        var html_td = `<a href="${url}" target="atcoder">${title}</a>`;
+        var tr = problem_tbody.insertRow(-1);
+        var td1 = tr.insertCell(-1);
+        td1.innerHTML = html_td;
+        if (problem_id !== undefined) {
+            var td3 = tr.insertCell(-1);
+            cells[problem_id] = td3;
+        }
+    }
+
+    // 提出状況を表示
+    if (atcoder_id.value !== '') {
+        const uid = atcoder_id.value
+        var url = "https://kenkoooo.com/atcoder/atcoder-api/results?user=" + uid;
+        fetch(url).then(function (response) {
+            response.text().then(function (text) {
+                const data = JSON.parse(text);
+                var AC = 0;
+                var score = {};
+                var epoch = [];
+                data.sort((x, y) => x.epoch_second - y.epoch_second);
+                for (var i = 0; i < data.length; i += 1) {
+                    var problem_id = data[i].problem_id;
+                    if (data[i].epoch_second >= 1663667515) {
+                        epoch.push(data[i].epoch_second);
+                    }
+                    else {
+                        continue;
+                    }
+                    //https://atcoder.jp/contests/abc029/submissions/21458111
+                    var contest_id = data[i].contest_id;
+                    var url = `https://atcoder.jp/contests/${contest_id}/submissions/${data[i].id}`
+                    if (problem_id in cells) {
+                        var s = '';
+                        if (data[i].result == 'AC') {
+                            s = `<span class="label-ac">AC</span>`;
+                            if (!(problem_id in score)) {
+                                score[problem_id] = 1;
+                                if (problem_id.endsWith('_d')) {
+                                    AC += 8;
+                                }
+                                else if (problem_id.endsWith('_c')) {
+                                    AC += 4;
+                                }
+                                else if (problem_id.endsWith('_b')) {
+                                    AC += 2;
+                                }
+                                else {
+                                    AC += 1;
+                                }
+                            }
+                        }
+                        else if (data[i].result == 'TLE') {
+                            s = '<span class="label-mod">TLE</span>';
+                        }
+                        else {
+                            s = '<span class="label-wa">' + data[i].result + '</span>';
+                        }
+                        cells[problem_id].innerHTML += `<a href="${url}" target="atcoder_result">${s}</a> `;
+                    }
+                }
+                //
+                epoch.sort();
+                //console.log(epoch);
+                var time = 0;
+                for (var i = 1; i < epoch.length; i += 1) {
+                    time += Math.min(epoch[i] - epoch[i - 1], 45 * 60);
+                }
+                time = (((time * 100) / 3600) | 0) / 100
+                //
+                var element = document.getElementById('results');
+                element.innerHTML = `<span class="label-ac">得点</span> ${AC}`
+                    + ` <span class="label-wa">練習時間(おおよそ)</span> ${time}`;
+            });
+        });
+    }
+}
+
+
